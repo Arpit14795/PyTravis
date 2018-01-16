030656693880554acae3ba1db8baae8338fd0d32
Arpit14795 <arpit_jain2@persistent.co.in>
2018-01-16 15:27:26 +0530

    without INSTALL

--- PyProCI/src/File1.py
+++ PyProCI/src/File1.py
@@ -1,25 +1,14 @@
-'''
-Created on Jan 15, 2018
+import unittest
 
-@author: arpit_jain2
-'''
-
-def tupleVarArgs(arg1, arg2='defaultB',*theRest):   #*theRest can cosume only non-keyword argumnet 
-    """varaible argumnets"""
-    print ('formal arg 1:', arg1)
-    print ('formal arg 2:', arg2)
-    print (theRest)                       #('xyz',456.789)
-    for eachXtrArg in theRest:
-        print ('another arg:', eachXtrArg) 
-   
-    
-#calling functions        
-tupleVarArgs('abc', 123, 'xyz', 456.789)       #this works       theRest = ('xyz', 456.789)
-print ("----------------------------------------------------")
-tupleVarArgs('abc', 123, [1,2,3],['A','b'])    #works    theRest =([1,2,3],['A','b'])
-#tupleVarArgs('abc',123, arg4 = 'xyz')         #error
-print ("----------------------------------------------------")
-tupleVarArgs('abc')                            #this works *theRest tuple willbe empty
-
-print ("----------------------------------------------------")
-
+class Trav(unittest.TestCase):
+	def setUp(self):
+		print ('inside set up file')
+		
+	def setUp_2(self):
+		print ('inside set up 2 file')
+		
+	def setUp_3(self):
+		print ('inside set up 3 file')
+		
+if __name__=="__main__":
+	unittest.main()
